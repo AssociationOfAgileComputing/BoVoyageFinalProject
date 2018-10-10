e8f7e4a89ebcdf3f235e98bd3dc1a2852e2a7aa4
Cris <cristtianopinto@gmail.com>
2018-10-10 18:16:36 +0200

    Correction - le attribut [StringLength(16)] cause problem si utiliser dans un decimal

--- BoVoyageFinalProject/Models/BookingFile.cs
+++ BoVoyageFinalProject/Models/BookingFile.cs
@@ -7,88 +7,82 @@ using System.Web;
 
 namespace BoVoyageFinalProject.Models
 {
-	public class BookingFile : BaseModel
+    public class BookingFile : BaseModel
 
-	{
-		public int CustomerId { get; set; }
-		[ForeignKey("CustomerId")]
-		public Customer Customer { get; set; }
+    {
+        public int CustomerId { get; set; }
 
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
 
-		public int TravellerId { get; set; }
-		[ForeignKey("TravellerId")]
-		public Traveller Traveller { get; set; }
+        public int TravellerId { get; set; }
 
-		public int TravelId { get; set; }
-		[ForeignKey("TravelId")]
-		public Travel Travel { get; set; }
+        [ForeignKey("TravellerId")]
+        public Traveller Traveller { get; set; }
 
-		[Required]
-		[Display(Name = "Numéro de carte de crédit")]
-		[StringLength(16)]
-		//[DataType(DataType.CreditCard)]
-		public string CreditCardNumber { get; set; }
+        public int TravelId { get; set; }
 
+        [ForeignKey("TravelId")]
+        public Travel Travel { get; set; }
 
-		[Required]
-		[Display(Name = "Prix par personne")]
-		[StringLength(6)]
-		public decimal PricePerPerson { get; set; }
+        [Required]
+        [Display(Name = "Numéro de carte de crédit")]
+        [StringLength(16)]
+        //[DataType(DataType.CreditCard)]
+        public string CreditCardNumber { get; set; }
 
-	
-		[Display(Name = "Prix total")]
-		[StringLength(10)]
-		public decimal TotalPrice { get; set; }
+        [Required]
+        [Display(Name = "Prix par personne")]
+        public decimal PricePerPerson { get; set; }
 
-		
-		[Display(Name = "Nombre de participants")]
-		[StringLength(2)]
-		public int TravellersNumber { get; set; }
+        [Display(Name = "Prix total")]
+        [StringLength(10)]
+        public decimal TotalPrice { get; set; }
 
-		public int? InsuranceId { get; set; }
-		[ForeignKey("InsuranceId")]
-		public Insurance Insurance { get; set; }
+        [Display(Name = "Nombre de participants")]
+        [StringLength(2)]
+        public int TravellersNumber { get; set; }
 
-		public enum BookingFileState
-		{
-			PENDING,
-			INPROGRESS,
-			DENIED,
-			GRANTED
-		}
+        public int? InsuranceId { get; set; }
 
-		public enum BookingFileCancellationReason
-		{
-			CUSTOMER,
-			UNAVAILABLEPLACES
-		}
+        [ForeignKey("InsuranceId")]
+        public Insurance Insurance { get; set; }
 
-		public void GetTotalPrice()
-		{
+        public enum BookingFileState
+        {
+            PENDING,
+            INPROGRESS,
+            DENIED,
+            GRANTED
+        }
 
-			this.TotalPrice = ((this.PricePerPerson * TravellersNumber));
-		}
+        public enum BookingFileCancellationReason
+        {
+            CUSTOMER,
+            UNAVAILABLEPLACES
+        }
 
-		public void CheckSolvency()
-		{
-		}
+        public void GetTotalPrice()
+        {
+            this.TotalPrice = ((this.PricePerPerson * TravellersNumber));
+        }
 
-		public void GrantBookingFile()
-		{
+        public void CheckSolvency()
+        {
+        }
 
-		}
+        public void GrantBookingFile()
+        {
+        }
 
-		public void BookingCancellation(BookingFileCancellationReason reason)
-		{
+        public void BookingCancellation(BookingFileCancellationReason reason)
+        {
+        }
 
-		}
+        public ICollection<Customer> Customers { get; set; }
 
-		public ICollection<Customer> Customers { get; set; }
+        public ICollection<Traveller> Travellers { get; set; }
 
-		public ICollection<Traveller> Travellers { get; set; }
-
-		public ICollection<Insurance> Insurances { get; set; }
-
-
-	}
+        public ICollection<Insurance> Insurances { get; set; }
+    }
 }
