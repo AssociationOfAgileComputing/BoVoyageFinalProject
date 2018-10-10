caae1765e2e70f9bde26ab9405daf0d8a6ddf4d1
AlexinaO <alexina.orsoni@gmail.com>
2018-10-10 16:45:05 +0200

    Ajout des Propriétés et des Attributs à la classe Customer

--- BoVoyageFinalProject/Models/Customer.cs
+++ BoVoyageFinalProject/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,22 @@ namespace BoVoyageFinalProject.Models
 {
     public class Customer : Person
     {
+        [Required(ErrorMessage ="Le champ{0} est obligatoire.")]
+        [Display(Name ="Date de naissance")]
+        [DataType(DataType.Date)]
+        [Column(TypeName ="datetime2")]
+        public DateTime BirthDate { get; set; }
+
+        [CreditCard(ErrorMessage="Numéro de carte invalide.")]
+        public string CreditCardNumber { get; set; }
+
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name ="Dossiers de réservation")]
+        public ICollection<BookingFile>BookingFiles { get; set; }
+
     }
 }
