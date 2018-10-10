7d13b3e68ae92183831585bae525c398ffc3143a
AlexinaO <alexina.orsoni@gmail.com>
2018-10-10 17:07:38 +0200

    Ajout des propriétés et attributs pour la classe traveller

--- BoVoyageFinalProject/Models/Traveller.cs
+++ BoVoyageFinalProject/Models/Traveller.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,17 @@ namespace BoVoyageFinalProject.Models
 {
     public class Traveller:Person
     {
+        [Required(ErrorMessage = "Le champ{0} est obligatoire.")]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "datetime2")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Dossiers de réservation")]
+        public ICollection<BookingFile> BookingFiles { get; set; }
     }
 }
