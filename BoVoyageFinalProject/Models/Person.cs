7e311382b269123f5d12d0e31ebaab7a9ccf6a77
AlexinaO <alexina.orsoni@gmail.com>
2018-10-10 22:52:13 +0200

    Suppression des propriétés Email, Password et ConfirmedPassword de la classe Person

--- BoVoyageFinalProject/Models/Person.cs
+++ BoVoyageFinalProject/Models/Person.cs
@@ -24,28 +24,5 @@ namespace BoVoyageFinalProject.Models
         [RegularExpression(@"^[A-Z][a-z\D\-\'][^$@#^%§!\p{P}\*""]+$")]
         public string LastName { get; set; }
 
-
-        [Index(IsUnique = true)]
-        [EmailAddress(ErrorMessage = "Le mail existe déjà.")]
-        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
-        [StringLength(150, ErrorMessage = "Le champ {0} doit contenir {1} caractères maximum.")]
-        [Display(Name = "Adresse E-mail")]
-        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Le format n'est pas bon.")]
-        public string Email { get; set; }
-
-        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
-        [Display(Name = "Mot de Passe")]
-        [DataType(DataType.Password)]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$", ErrorMessage = "{0} incorrect.")]
-        [StringLength(150)]
-        public string Password { get; set; }
-
-
-        [NotMapped]
-        [Display(Name = "Confirmation du mot de passe")]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage ="La confirmation n'est pas bonne.")]
-        public string ConfirmedPassword { get; set; }
-
     }
 }
