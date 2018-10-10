80929cf99b6fab038bebd1df40150b35a9f9ef2d
anneprt <anne.parrot82@gmail.com>
2018-10-10 16:24:03 +0200

    implementation des propriétés et méthodes dans la classe BookingFile

--- BoVoyageFinalProject/Models/BookingFile.cs
+++ BoVoyageFinalProject/Models/BookingFile.cs
@@ -1,11 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageFinalProject.Models
 {
-	public class BookingFile
+	public class BookingFile : BaseModel
+
 	{
+		public int CustomerId { get; set; }
+		[ForeignKey("CustomerId")]
+		public Customer Customer { get; set; }
+
+
+		public int TravellerId { get; set; }
+		[ForeignKey("TravellerId")]
+		public Traveller Traveller { get; set; }
+
+		public int TravelId { get; set; }
+		[ForeignKey("TravelId")]
+		public Travel Travel { get; set; }
+
+		public string CreditCardNumber { get; set; }
+
+		public decimal PricePerPerson { get; set; }
+
+		public decimal TotalPrice { get; set; }
+
+		public int TravellersNumber { get; set; }
+
+		public int? InsuranceId { get; set; }
+		[ForeignKey("InsuranceId")]
+		public Insurance Insurance { get; set; }
+
+		public enum BookingFileState
+		{
+			PENDING,
+			INPROGRESS,
+			DENIED,
+			GRANTED
+		}
+
+		public enum BookingFileCancellationReason
+		{
+			CUSTOMER,
+			UNAVAILABLEPLACES
+		}
+
+		public void GetTotalPrice()
+		{
+
+			this.TotalPrice = ((this.PricePerPerson * TravellersNumber));
+		}
+
+		public void CheckSolvency()
+		{
+		}
+
+		public void GrantBookingFile()
+		{
+
+		}
+
+		public void BookingCancellation(BookingFileCancellationReason reason)
+		{
+
+		}
+
 	}
 }
