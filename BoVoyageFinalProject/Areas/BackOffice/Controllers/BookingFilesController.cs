901b384fc1a3f416674c3d0fa6e926dc58b0cc87
anneprt <anne.parrot82@gmail.com>
2018-10-10 22:56:57 +0200

    création de BookingFilesController

---
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/BookingFilesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
+{
+    public class BookingFilesController : Controller
+    {
+        // GET: BackOffice/BookingFiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
