e5a941f5c3fdf84436b746b135c7707562affce4
anneprt <anne.parrot82@gmail.com>
2018-10-10 22:57:52 +0200

    creation de DashboardController

---
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: BackOffice/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
