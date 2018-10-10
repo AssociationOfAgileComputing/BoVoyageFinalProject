8df6b95e15f014b3b277aa3ce8b06c994f9decf3
Cris <cristtianopinto@gmail.com>
2018-10-10 16:32:33 +0200

    Ajout des propriétés dans la classe Travel

--- BoVoyageFinalProject/Models/Travel.cs
+++ BoVoyageFinalProject/Models/Travel.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageFinalProject.Models
 {
-    public class Travel
+    public class Travel : BaseModel
     {
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date Aller")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:MM}")]
+        public DateTime DateGo { get; set; }
+
+        [Required]
+        [Display(Name = "Date retour")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:MM}")]
+        public DateTime DateBack { get; set; }
+
+        [Required]
+        [Display(Name = "Place disponible")]
+        public int SpaceAvailable { get; set; }
+
+        [Display(Name = "Prix")]
+        public decimal? Price { get; set; }
+
+        public List<BookingFile> BookingFiles { get; set; }
+
+        public int TravelAgencyID { get; set; }
+
+        [ForeignKey("TravelAgencyID")]
+        public TravelAgency TravelAgency { get; set; }
+
+        public int DestinationID { get; set; }
+
+        [ForeignKey("DestinationID")]
+        public Destination Destination { get; set; }
     }
 }
