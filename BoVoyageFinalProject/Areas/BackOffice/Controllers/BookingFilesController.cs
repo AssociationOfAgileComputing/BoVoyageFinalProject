8768fdeadc8e950b78dc9308b6eb4b09b0a6649d
anneprt <anne.parrot82@gmail.com>
2018-10-11 00:34:40 +0200

    création de la methode ActionResult Registration dans le BookingFilesController

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/BookingFilesController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/BookingFilesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoVoyageFinalProject.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,17 @@ using System.Web.Mvc;
 
 namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
 {
-    public class BookingFilesController : Controller
+    public class BookingFilesController : BaseController
     {
         // GET: BackOffice/BookingFiles
-        public ActionResult Index()
+        public ActionResult Registration()
         {
+            var insurances = db.Insurances.ToList();
+            ViewBag.Insurances = insurances.Select(x => new SelectListItem
+            {
+                Text = x.InsuranceType.ToString(),
+                Value = x.InsuranceCost.ToString()
+            });
             return View();
         }
     }
