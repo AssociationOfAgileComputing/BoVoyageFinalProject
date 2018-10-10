b8d6b440137d1c85a18b6272688a6a6b72df9f96
Cris <cristtianopinto@gmail.com>
2018-10-10 16:32:04 +0200

    Ajout des propriétés dans la classeDestination

--- BoVoyageFinalProject/Models/Destination.cs
+++ BoVoyageFinalProject/Models/Destination.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageFinalProject.Models
 {
-    public class Destination
+    public class Destination : BaseModel
     {
+        [Required]
+        [Display(Name = "Continent")]
+        public string Continent { get; set; }
+
+        [Required]
+        [Display(Name = "Pays")]
+        public string Pays { get; set; }
+
+        [Required]
+        [Display(Name = "Region")]
+        public string Region { get; set; }
+
+        [StringLength(250)]
+        [Display(Name = "description")]
+        public string Description { get; set; }
+
+        public List<Travel> Travels { get; set; }
+
+        [Display(Name = "Images")]
+        public ICollection<DestinationPicture> Pictures { get; set; }
     }
 }
