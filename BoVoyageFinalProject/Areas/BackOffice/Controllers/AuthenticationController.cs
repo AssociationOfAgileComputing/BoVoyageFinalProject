e4f793449857a52dffc84bbfc471609a9fe67ad6
AlexinaO <alexina.orsoni@gmail.com>
2018-10-11 14:14:09 +0200

    Modification de MANAGER en SALESMANAGER dans session pour AuthenticationController

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/AuthenticationController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -29,7 +29,7 @@ namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
                 }
                 else
                 {
-                    Session["MANAGER"] = manager;
+                    Session["SALESMANAGER"] = manager;
                     return RedirectToAction("Index", "Dashboard", new { area = "BackOffice" });
                 }
             }
@@ -38,7 +38,7 @@ namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
 
         public ActionResult Logout()
         {
-            Session.Remove("MANAGER");
+            Session.Remove("SALESMANAGER");
             return RedirectToAction("Index", "Home", new { area = "" });
         }
     }
