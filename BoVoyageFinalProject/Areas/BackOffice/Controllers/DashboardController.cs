200361895405c742857fc5f7caa24df672b69465
Cris <cristtianopinto@gmail.com>
2018-10-11 10:04:11 +0200

    Heritage BaseController

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoVoyageFinalProject.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
 {
-    public class DashboardController : Controller
+    public class DashboardController : BaseController
     {
         // GET: BackOffice/Home
         public ActionResult Index()
