7814b56a0c55845389cc6e927a5f9a54c3d3f40a
anneprt <anne.parrot82@gmail.com>
2018-10-11 15:20:55 +0200

    suppression BookingFilesController et génération InsurancesController

---
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/InsurancesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageFinalProject.Controllers;
+using BoVoyageFinalProject.Data;
+using BoVoyageFinalProject.Models;
+
+namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
+{
+    public class InsurancesController : BaseController
+    {
+
+        // GET: BackOffice/Insurances
+        public ActionResult Index()
+        {
+            return View(db.Insurances.ToList());
+        }
+
+        // GET: BackOffice/Insurances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // GET: BackOffice/Insurances/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackOffice/Insurances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,InsuranceCost,InsuranceType")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Insurances.Add(insurance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insurance);
+        }
+
+        // GET: BackOffice/Insurances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: BackOffice/Insurances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,InsuranceCost,InsuranceType")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insurance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insurance);
+        }
+
+        // GET: BackOffice/Insurances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: BackOffice/Insurances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insurance insurance = db.Insurances.Find(id);
+            db.Insurances.Remove(insurance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
