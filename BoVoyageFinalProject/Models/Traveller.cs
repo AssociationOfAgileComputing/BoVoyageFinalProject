d2ddca578d93eac6665f25b61a6998325f796af2
AlexinaO <alexina.orsoni@gmail.com>
2018-10-12 02:52:24 +0200

    Ajout de l'email (non requis) à la classe Traveller

--- BoVoyageFinalProject/Models/Traveller.cs
+++ BoVoyageFinalProject/Models/Traveller.cs
@@ -9,15 +9,22 @@ namespace BoVoyageFinalProject.Models
 {
     public class Traveller:Person
     {
-        [Required(ErrorMessage = "Le champ{0} est obligatoire.")]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
         [Display(Name = "Date de naissance")]
         [DataType(DataType.Date)]
         [Column(TypeName = "datetime2")]
         public DateTime BirthDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Le champ {0} est obligatoire."]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+
+        [Index(IsUnique = true)]
+        [EmailAddress(ErrorMessage = "Le mail existe déjà.")]
+        [StringLength(150, ErrorMessage = "Le champ {0} doit contenir {1} caractères maximum.")]
+        [Display(Name = "Adresse E-mail")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Le format n'est pas bon.")]
+        public string Mail { get; set; }
 
         [Display(Name = "Dossiers de réservation")]
         public ICollection<BookingFile> BookingFiles { get; set; }
