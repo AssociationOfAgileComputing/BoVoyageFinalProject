38ef8a429e5a132c97f44576cf7b0bd2120f7200
anneprt <anne.parrot82@gmail.com>
2018-10-12 18:04:43 +0200

    ajouts barre de recherche client dans le dashboard

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using BoVoyageFinalProject.Controllers;
 using BoVoyageFinalProject.Filters;
+using BoVoyageFinalProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,29 @@ namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
 {
 	[Authentication]
 	public class DashboardController : BaseController
-    {
-        // GET: BackOffice/Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	{
+		// GET: BackOffice/Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult SearchCustomer(string LastName)
+		{
+			var customer = db.Customers.Where(x => x.LastName == LastName).ToList();
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			if (customer.Count == 0)
+			{
+				Display("Il n'existe pas de client correspondant à votre recherche");
+				return RedirectToAction("Index");
+			}
+			return View(customer);
+
+		}
+
+
+	}
 }
