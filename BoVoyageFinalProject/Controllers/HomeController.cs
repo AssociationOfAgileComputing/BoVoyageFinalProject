9691a9324663905c6f7858aadae854521f75143a
Cris <cristtianopinto@gmail.com>
2018-10-12 10:01:15 +0200

    Ajout methode details

--- BoVoyageFinalProject/Controllers/HomeController.cs
+++ BoVoyageFinalProject/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BoVoyageFinalProject.Models;
 using System.Data.Entity;
+using System.Net;
 
 namespace BoVoyageFinalProject.Controllers
 {
@@ -35,6 +36,20 @@ namespace BoVoyageFinalProject.Controllers
             ViewBag.Message = "Contacter le service commercial";
 
             return View();
+        }
+      
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").ToList();
+            if (travels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(travels);
         }
     }
 }
