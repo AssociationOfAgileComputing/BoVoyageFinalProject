200491abe3dcfa9077bcc226c43a2038dc9f95c6
anneprt <anne.parrot82@gmail.com>
2018-10-12 12:46:21 +0200

    suppression de ContactController (inutile) et modifications (ajout participants) dans la vue create de BookingFiles,ajout du formulaire dans la vue Contact de Home

--- BoVoyageFinalProject/Controllers/HomeController.cs
+++ BoVoyageFinalProject/Controllers/HomeController.cs
@@ -9,47 +9,52 @@ using System.Net;
 
 namespace BoVoyageFinalProject.Controllers
 {
-    public class HomeController : BaseController
-    {
-        public ActionResult Index()
-        {
-            var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").ToList();
-            return View(travels);
-        }
+	public class HomeController : BaseController
+	{
+		public ActionResult Index()
+		{
+			var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").ToList();
+			return View(travels);
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Bienvenue sur l'application BoVoyage";
-            var modelInfo = new Info
-            {
-                DevName = "Association of Agile Computing",
-                Address = "rue de la bonne Entente - Paris",
-                ContactMail = "a2c@gmail.com",
-                CreatedDate = "Octobre 2008"
-            };
+		public ActionResult About()
+		{
+			ViewBag.Message = "Bienvenue sur l'application BoVoyage";
+			var modelInfo = new Info
+			{
+				DevName = "Association of Agile Computing",
+				Address = "rue de la bonne Entente - Paris",
+				ContactMail = "a2c@gmail.com",
+				CreatedDate = "Octobre 2008"
+			};
 
-            return View(modelInfo);
-        }
+			return View(modelInfo);
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Contacter le service commercial";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Contacter le service commercial";
 
-            return View();
-        }
-        [Route("DetailVoyageRoute")]
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").SingleOrDefault(x => x.ID == id);
-            if (travels == null)
-            {
-                return HttpNotFound();
-            }
-            return View(travels);
-        }
-    }
+			
+
+			 Display("Merci de nous avoir contactés nous vous répondrons dans les meilleurs délais"); 
+
+			return View();
+		}
+
+		[Route("DetailVoyageRoute")]
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").SingleOrDefault(x => x.ID == id);
+			if (travels == null)
+			{
+				return HttpNotFound();
+			}
+			return View(travels);
+		}
+	}
 }
