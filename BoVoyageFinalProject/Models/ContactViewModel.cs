85ac599758a4aaee3d596ad4744053c56469782e
anneprt <anne.parrot82@gmail.com>
2018-10-12 02:35:46 +0200

    creation model view et controller pour formulaire de contact

---
+++ BoVoyageFinalProject/Models/ContactViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageFinalProject.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        [Display(Name = "Nom-Prénom")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Sujet")]
+        public string Subject { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Message { get; set; }
+    }
+}
