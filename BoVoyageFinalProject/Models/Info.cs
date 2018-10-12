4a1fc0304a21b7a04d7fc17f82a30c5955cc1026
AlexinaO <alexina.orsoni@gmail.com>
2018-10-12 02:17:14 +0200

    Ajout d'une classe Info pour avoir les paramètres pour les informations de la page à propos

---
+++ BoVoyageFinalProject/Models/Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageFinalProject.Models
+{
+    public class Info
+    {
+        public string DevName { get; set; }
+
+        public string CreatedDate { get; set; }
+
+        public string ContactMail { get; set; }
+
+        public string Address { get; set; }
+    }
+}
