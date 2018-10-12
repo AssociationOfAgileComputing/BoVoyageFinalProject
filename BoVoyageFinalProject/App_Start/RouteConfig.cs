4f612e78b250f91791ad4fd725538ba384f0d30f
AlexinaO <alexina.orsoni@gmail.com>
2018-10-12 15:51:01 +0200

    Action Edit de CustomersController

--- BoVoyageFinalProject/App_Start/RouteConfig.cs
+++ BoVoyageFinalProject/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,7 +8,7 @@ using System.Web.Routing;
 
 namespace BoVoyageFinalProject
 {
-    public class RouteConfig
+    public static class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
@@ -32,6 +33,7 @@ namespace BoVoyageFinalProject
                 url: "details-voyage{nom}",
                 defaults: new { controller = "Home", action = "Details", id = UrlParameter.Optional }
                 );
+
 
             routes.MapRoute(
                 name: "Default",
