c3e6b2f726d8707d652f287bd777200a06aa6ed0
anneprt <anne.parrot82@gmail.com>
2018-10-14 11:42:16 +0200

    Modification de la page d'accueil
    Seuls les tops et la recherche sont affichés sur la page d'accueil
    Quand on fait une recherche, on arrive sur la page Search avec les voyages trouvés
    Affichage en détail avec photo des voyages

--- BoVoyageFinalProject/Controllers/HomeController.cs
+++ BoVoyageFinalProject/Controllers/HomeController.cs
@@ -10,41 +10,41 @@ using BoVoyageFinalProject.Tools;
 
 namespace BoVoyageFinalProject.Controllers
 {
-	public class HomeController : BaseController
-	{
-		public ActionResult Index()
-		{
-			var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").OrderBy(x => x.DateGo).ToList();
-			return View(travels);
-		}
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var travels = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency").OrderBy(x => x.DateGo).ToList();
+            return View(travels);
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Bienvenue sur l'application BoVoyage";
-			var modelInfo = new Info
-			{
-				DevName = "Association of Agile Computing",
-				Address = "rue de la bonne Entente - Paris",
-				ContactMail = "a2c@gmail.com",
-				CreatedDate = "Octobre 2008"
-			};
+        public ActionResult About()
+        {
+            ViewBag.Message = "Bienvenue sur l'application BoVoyage";
+            var modelInfo = new Info
+            {
+                DevName = "Association of Agile Computing",
+                Address = "rue de la bonne Entente - Paris",
+                ContactMail = "a2c@gmail.com",
+                CreatedDate = "Octobre 2008"
+            };
 
-			return View(modelInfo);
-		}
+            return View(modelInfo);
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Contacter le service commercial";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contacter le service commercial";
 
             return View();
         }
 
-		public ActionResult SendMail()
-		{
-			Display("Merci de nous avoir contactés nous vous recontacterons dans les meilleurs délais");
-			return RedirectToAction("Index");
-		}
-        
+        public ActionResult SendMail()
+        {
+            Display("Merci de nous avoir contactés nous vous recontacterons dans les meilleurs délais");
+            return RedirectToAction("Index");
+        }
+
         [Route("voyage-{region}-{pays}/{id}")]
         public ActionResult Details(int? id)
         {
@@ -59,53 +59,49 @@ namespace BoVoyageFinalProject.Controllers
             }
             return View(travels);
         }
-        
-        public ActionResult Search(string pays)
+
+        public ActionResult Search(string pays, decimal? prixMin, decimal? prixMax, DateTime? dateAller, DateTime? dateRetour)
         {
-            var travels = db.Travels.Include(t => t.Destination)
-                .Include(t => t.Destination.Pictures)
-                .Include("TravelAgency").Where(x=>x.Destination.Country == pays).ToList();
+            var query = db.Travels.Include(t => t.Destination).Include(t => t.Destination.Pictures).Include("TravelAgency");
+
+            // Search by pays
+            if (!string.IsNullOrWhiteSpace(pays))
+            {
+                query = query.Where(x => x.Destination.Country == pays);
+            }
+
+            // Search by priceMin
+            if (prixMin != null)
+            {
+                query = query.Where(x => x.Price >= prixMin);
+            }
+
+            // Search by priceMax
+            if (prixMax != null)
+            {
+                query = query.Where(x => x.Price <= prixMax);
+            }
+
+            // Search by dateAller
+            if (dateAller != null)
+            {
+                query = query.Where(x => x.DateGo >= dateAller);
+            }
+
+            // Search by dateRetour
+            if (dateRetour != null)
+            {
+                query = query.Where(x => x.DateBack <= dateRetour);
+            }
+
+            var travels = query.ToList();
+
+
             if (travels == null)
             {
                 return HttpNotFound();
             }
-            if (travels.Count==0)
-            {
-                Display("Malheureusement, nous n'avons pas de voyages à cette destination");
-                return RedirectToAction("Index");
-            }
-            return View(travels);
-        }
-        public ActionResult SearchPrice(decimal? prixMin, decimal? prixMax)
-        {
-            var travels = db.Travels.Include(t => t.Destination)
-                .Include(t => t.Destination.Pictures)
-                .Include("TravelAgency").Where(x => x.Price >= prixMin && x.Price <= prixMax).ToList();
-            if (travels == null)
-            {
-                return HttpNotFound();
-            }
-            if (travels.Count == 0)
-            {
-                Display("Malheureusement, nous n'avons pas de voyages dans votre gamme de prix");
-                return RedirectToAction("Index");
-            }
-            return View(travels);
-        }
-        public ActionResult SearchDate(DateTime? dateAller, DateTime? dateRetour)
-        {
-            var travels = db.Travels.Include(t => t.Destination)
-                .Include(t => t.Destination.Pictures)
-                .Include("TravelAgency").Where(x => x.DateGo >= dateAller && x.DateBack <= dateRetour).ToList();
-            if (travels == null)
-            {
-                return HttpNotFound();
-            }
-            if (travels.Count == 0)
-            {
-                Display("Malheureusement, nous n'avons pas de voyages à ces dates");
-                return RedirectToAction("Index");
-            }
+
             return View(travels);
         }
 
@@ -122,7 +118,7 @@ namespace BoVoyageFinalProject.Controllers
             {
                 return View();
             }
-            
+
         }
 
         [HttpPost]
@@ -143,7 +139,7 @@ namespace BoVoyageFinalProject.Controllers
                 db.SaveChanges();
                 Session["Travellers"] = bookingFile.TravellersNumber;
                 Display("Le voyage a été créé. Ajoutez maintenant les participants.");
-                return RedirectToAction("AddTravellers", new { id = bookingFile.ID});//Rediriger vers ajout participant
+                return RedirectToAction("AddTravellers", new { id = bookingFile.ID });//Rediriger vers ajout participant
             }
             ViewBag.travel = db.Travels.SingleOrDefault(x => x.ID == id);
             return View();
@@ -158,13 +154,13 @@ namespace BoVoyageFinalProject.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult AddTravellers(int? id, List<Traveller> travellers)
         {
-                BookingFile bookingFile = db.BookingFiles.Include(x => x.Travel).SingleOrDefault(x => x.ID == id);
+            BookingFile bookingFile = db.BookingFiles.Include(x => x.Travel).SingleOrDefault(x => x.ID == id);
 
-                if (ModelState.IsValid)
+            if (ModelState.IsValid)
+            {
+                if (bookingFile.IsCustomerTraveller)
                 {
-                    if (bookingFile.IsCustomerTraveller)
-                {
-                    Customer user = (Customer) Session["CUSTOMER"];
+                    Customer user = (Customer)Session["CUSTOMER"];
                     Traveller traveller = new Traveller
                     {
                         FirstName = user.FirstName,
@@ -176,33 +172,33 @@ namespace BoVoyageFinalProject.Controllers
                     };
                     travellers.Add(traveller);
                 }
-                    foreach (Traveller t in travellers)
-                    {
-                            var traveller = db.Travellers.SingleOrDefault(x => x.Mail == t.Mail);
-                            if (traveller == null)
-                            {
-                                traveller = t;
-                                db.Travellers.Add(traveller);
-                                db.SaveChanges();
-                            }
-
-                            traveller.BookingFiles.Add(bookingFile);
-
-                            bookingFile.Travellers.Add(traveller);
-                            bookingFile.Travel.SpaceAvailable--;
-                            db.Entry(bookingFile).State = EntityState.Modified;
-                    }
-                    bookingFile.GetTotalPrice();
-                    db.SaveChanges();
-                    Display($"Ajout des participants pour le dossier de réservation {bookingFile.ID} effectué avec succès.");
-                    return RedirectToAction("Index", "Home");
-                }
-                else
+                foreach (Traveller t in travellers)
                 {
-                    db.BookingFiles.Remove(bookingFile);
-                    db.SaveChanges();
-                    return View(travellers);
+                    var traveller = db.Travellers.SingleOrDefault(x => x.Mail == t.Mail);
+                    if (traveller == null)
+                    {
+                        traveller = t;
+                        db.Travellers.Add(traveller);
+                        db.SaveChanges();
+                    }
+
+                    traveller.BookingFiles.Add(bookingFile);
+
+                    bookingFile.Travellers.Add(traveller);
+                    bookingFile.Travel.SpaceAvailable--;
+                    db.Entry(bookingFile).State = EntityState.Modified;
                 }
+                bookingFile.GetTotalPrice();
+                db.SaveChanges();
+                Display($"Ajout des participants pour le dossier de réservation {bookingFile.ID} effectué avec succès.");
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                db.BookingFiles.Remove(bookingFile);
+                db.SaveChanges();
+                return View(travellers);
             }
         }
     }
+}
