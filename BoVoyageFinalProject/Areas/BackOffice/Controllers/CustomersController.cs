afec5262b79d846a3eba13cbb84202bf6ef47651
anneprt <anne.parrot82@gmail.com>
2018-10-14 14:55:53 +0200

    Ajout de l'export au format CSV des clients pour le mailing au niveau du backoffice

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/CustomersController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using BoVoyageFinalProject.Controllers;
@@ -36,6 +37,18 @@ namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
                 return HttpNotFound();
             }
             return View(customer);
+        }
+
+        public FileContentResult CSV()
+        {
+            List<Customer> customers = db.Customers.ToList();
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"nom;prenom;email\n");
+            foreach (Customer customer in customers)
+            {
+                builder.Append($"{customer.LastName};{customer.FirstName};{customer.Mail}\n");
+            }
+            return File(new System.Text.UTF8Encoding().GetBytes(builder.ToString()), "text/csv", string.Format("export-{0}.csv", DateTime.Now.ToString("yyyyMMdd-HHmmss")));
         }
 
     }
