01b756d9e6234291daa68b51099721f905a4e9fd
anneprt <anne.parrot82@gmail.com>
2018-10-14 20:09:00 +0200

    Ajout du tableau de bord du commercial dans le backoffice
    10 prochains voyages à partir de la date du jour
    10 dossiers de réservations en attente pour les prochains voyages

--- BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
+++ BoVoyageFinalProject/Areas/BackOffice/Controllers/DashboardController.cs
@@ -3,39 +3,57 @@ using BoVoyageFinalProject.Filters;
 using BoVoyageFinalProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BoVoyageFinalProject.Areas.BackOffice.Controllers
 {
-	[Authentication]
-	public class DashboardController : BaseController
-	{
-		// GET: BackOffice/Home
-		public ActionResult Index()
-		{
-			return View();
-		}
+    [Authentication]
+    public class DashboardController : BaseController
+    {
+        // GET: BackOffice/Home
+        public ActionResult Index()
+        {
+            var model = new DashboardIndexViewModel();
+            // Recherche des 10 prochains voyages
+            model.NextTravels = db.Travels
+                .Include(x => x.Destination)
+                .Include(x => x.TravelAgency)
+                .Where(x => x.DateGo >= DateTime.Now)
+                .OrderBy(x => x.DateGo).Take(10).ToList();
 
-		public ActionResult SearchCustomer(string LastName)
-		{
-			var customer = db.Customers.Where(x => x.LastName == LastName).ToList();
-			if (customer == null)
-			{
-				return HttpNotFound();
-			}
-			if (customer.Count == 0)
-			{
-				Display("Il n'existe pas de client correspondant à votre recherche");
-				return RedirectToAction("Index");
-			}
-			return View(customer);
+            // Recherche des dossiers de réservations en attente pour les prochains voyages
+            model.BookingFilesWaitingStatus = db.BookingFiles
+                .Include(x => x.Customer)
+                .Include(x => x.Insurances)
+                .Include(x => x.Travellers)
+                .Include(x => x.Travel.Destination)
+                .Where(x => x.Travel.DateGo >= DateTime.Now && x.BookingFileState == "EN ATTENTE")
+                .OrderBy(x => x.Travel.DateGo).Take(10).ToList();
+            return View(model);
+        }
 
-		
+        public ActionResult SearchCustomer(string LastName)
+        {
+            var customer = db.Customers.Where(x => x.LastName == LastName).ToList();
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            if (customer.Count == 0)
+            {
+                Display("Il n'existe pas de client correspondant à votre recherche");
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+    }
 
-		}
-
-
-	}
+    public class DashboardIndexViewModel
+    {
+        public IEnumerable<Travel> NextTravels { get; set; }
+        public IEnumerable<BookingFile> BookingFilesWaitingStatus { get; set; }
+    }
 }
