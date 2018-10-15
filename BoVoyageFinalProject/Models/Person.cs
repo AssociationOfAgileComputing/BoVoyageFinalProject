cf488993d27c7198d37faa956db32343e6f7d722
S <S@gmail.com>
2018-10-15 14:55:35 +0200

    Modification de la Regex Nom et Prénom et ajout d'un message d'erreur explicite

--- BoVoyageFinalProject/Models/Person.cs
+++ BoVoyageFinalProject/Models/Person.cs
@@ -14,14 +14,14 @@ namespace BoVoyageFinalProject.Models
         [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
         [Display(Name = "Prénom")]
         [StringLength(50)]
-        [RegularExpression(@"^[A-Z][a-z\D\-\'][^$@#^%§!\*""]+$")]
+        [RegularExpression(@"^[A-Z][a-z^\d\-\'^($@#^%§!\*""\p{P})]+$", ErrorMessage = "Le nom doit contenir au minimum 2 lettres, commencer par une majuscule et ne pas contenir de caractères spéciaux")]
         public string FirstName { get; set; }
 
 
         [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
         [Display(Name = "Nom")]
         [StringLength(50)]
-        [RegularExpression(@"^[A-Z][a-z\D\-\'][^$@#^%§!\*""]+$")]
+        [RegularExpression(@"^[A-Z][a-z^\d\-\'^($@#^%§!\*""\p{P})]+$",ErrorMessage="Le nom doit contenir au minimum 2 lettres, commencer par une majuscule et ne pas contenir de caractères spéciaux")]
         public string LastName { get; set; }
 
         [NotMapped]
